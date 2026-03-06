ddec680ace6242d5e8e5c68711bb40665d37a66a
Ramil <Ramilmys@gmail.com>
2026-03-06 20:05:59 +0700

    Добавлены два вспомогательных компонента для раздельного отображения персонажей

---
+++ src/components/ApiCharactersList.jsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import { useCharacters, getCharacters } from "../store/characters";
+import styles from "../styles/character-catalog.module.css";
+import CharactersCard from "./CharactersCard";
+import { useShallow } from "zustand/shallow";
+
+function select(state) {
+  if (state.localFilters.sorting === "Name A-Z") {
+    return state.characters.toSorted((a, b) => a.name.localeCompare(b.name));
+  }
+  if (state.localFilters.sorting === "Name Z-A") {
+    return state.characters.toSorted((a, b) => b.name.localeCompare(a.name));
+  }
+  return state.characters;
+}
+
+export default function ApiCharactersList({ handleFavoriteClick, handleActiveCard }) {
+  const characters = useCharacters(useShallow(select));
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [characters]);
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  return (
+    <div className={styles["characters-list"]}>
+      {characters.length === 0 ? (
+        <div className={styles["error_characters-undefined"]}>Characters not found</div>
+      ) : (
+        characters.map((character) => {
+          return (
+            <CharactersCard
+              key={character.id}
+              character={character}
+              handleFavoriteClick={handleFavoriteClick}
+              handleActiveCard={handleActiveCard}
+            />
+          );
+        })
+      )}
+    </div>
+  );
+}
