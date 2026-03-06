e9373c2cd34f658fed5e888478ba04de74caca7c
Ramil <Ramilmys@gmail.com>
2026-03-06 20:24:43 +0700

    Добавлены два вспомогателных компонента для отображения элементов страницы для разных видом персонажей

---
+++ src/components/SearchApiCharacters.jsx
@@ -0,0 +1,39 @@
+import ReloadIcon from "../icons/ReloadIcon";
+import SearchIcon from "../icons/SearchIcon";
+import { useCharacters, setName, setUrlPage } from "../store/characters";
+import styles from "../styles/character-catalog.module.css";
+import Button from "./Button";
+
+export default function SearcApihCharacters() {
+  const name = useCharacters((state) => state.name);
+  const urlPage = useCharacters((state) => state.urlPage);
+
+  return (
+    <div className={styles["search-container"]}>
+      <div className={styles["search-field"]}>
+        <SearchIcon className={styles["search-icon"]} />
+
+        <input
+          type={"text"}
+          className={styles["input-search"]}
+          placeholder={"Search by name or type..."}
+          value={name}
+          onChange={(e) => {
+            if (urlPage > 1) setUrlPage(1);
+            setName(e.target.value);
+          }}
+        />
+      </div>
+
+      <Button
+        disabled={name === "" ? true : false}
+        btnClassName={name === "" ? styles["reset-btn-disabled"] : styles["reset-btn"]}
+        Icon={ReloadIcon}
+        onClick={() => {
+          if (urlPage > 1) setUrlPage(1);
+          setName("");
+        }}
+      />
+    </div>
+  );
+}
