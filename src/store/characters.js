7f0b1d21a1fc6aeb2e5b446aaf557d93a346a21f
Ramil <Ramilmys@gmail.com>
2026-03-06 20:04:59 +0700

    Добавлены все необходимые сторы и прописаны экшены

--- src/store/characters.js
+++ src/store/characters.js
@@ -1,59 +1,142 @@
 import { create } from "zustand";
 
-export const useCharacters = create((set, get) => ({
+export const statusOptions = ["All status", "Alive", "Dead", "Unknown"];
+export const genderOptions = ["All gender", "Male", "Female", "Genderless", "Unknown"];
+export const speciesOptions = [
+  "All species",
+  "Human",
+  "Alien",
+  "Humanoid",
+  "Poopybutthole",
+  "Mythological Creature",
+  "Animal",
+  "Robot",
+  "Cronenberg",
+  "Disease",
+  "Unknown",
+];
+export const sortingOptions = ["Without sorting", "Name A-Z", "Name Z-A"];
+
+const defApiFilters = {
+  status: "All status",
+  gender: "All gender",
+  species: "All species",
+};
+
+const defLocalFilters = {
+  sorting: "Without sorting",
+};
+
+export const useCharacters = create(() => ({
+  activeCard: null,
+  isActive: false,
+  timerId: null,
   characters: [],
   info: null,
+  apiFilters: defApiFilters,
+  localFilters: defLocalFilters,
+  name: "",
+  statusOptions,
+  genderOptions,
+  speciesOptions,
+  sortingOptions,
 
   totalPages: 0,
   urlPage: 1,
-
-  setTotalPages: (totalPages) => set({ totalPages }),
-  setUrlPage: (urlPage) => set({ urlPage }),
-
-  filters: {
-    name: "",
-    status: "All status",
-    gender: "All gender",
-    species: "All species",
-    sorting: "Without sorting",
-  },
-
-  setFilter: (key, value) =>
-    set((state) => ({
-      filters: { ...state.filters, [key]: value },
-    })),
-
-  getCharacters: async () => {
-    const { urlPage, filters } = get();
-
-    const { name, status, gender, species, sorting } = filters;
-
-    const urlPage1 = urlPage * 2 - 1;
-    const urlPage2 = urlPage * 2;
-
-    const querys = `${name ? `&name=${name}` : ""}${status === "All status" ? "" : `&status=${status}`}${gender === "All gender" ? "" : `&gender=${gender}`}${species === "All species" ? "" : `&species=${species}`}`;
-
-    const urls = [
-      `https://rickandmortyapi.com/api/character?page=${urlPage1}${querys}`,
-      `https://rickandmortyapi.com/api/character?page=${urlPage2}${querys}`,
-    ];
-
-    const responses = await Promise.all(urls.map((url) => fetch(url)));
-
-    if (responses.some((response) => !response.ok)) {
-      set({ characters: [] });
-      set({ info: null });
-      set({ totalPages: 0 });
-
-      return;
-    }
-
-    const data = await Promise.all(responses.map((res) => res.json()));
-    const allCharacters = data.flatMap((page) => (page.results ? page.results : []));
-    const allInfo = data[0].info;
-
-    set({ characters: allCharacters });
-    set({ info: allInfo });
-    set({ totalPages: Math.ceil(allInfo?.count / 40) });
-  },
 }));
+
+export function setIsActive(isActive) {
+  useCharacters.setState({ isActive });
+}
+
+export function setActiveCard(activeCard) {
+  useCharacters.setState({ activeCard });
+}
+
+export function setName(name) {
+  const { timerId } = useCharacters.getState();
+  useCharacters.setState({ name });
+
+  if (timerId) {
+    clearTimeout(timerId);
+  }
+
+  const newTimerId = setTimeout(() => {
+    getCharacters();
+  }, 1000);
+
+  useCharacters.setState({ timerId: newTimerId });
+}
+
+export function setUrlPage(urlPage) {
+  useCharacters.setState({ urlPage });
+  getCharacters();
+}
+
+export function setApiFilter(key, value) {
+  useCharacters.setState((state) => ({
+    apiFilters: { ...state.apiFilters, [key]: value },
+  }));
+
+  getCharacters();
+}
+
+export function setLocalFilter(key, value) {
+  useCharacters.setState((state) => ({
+    localFilters: { ...state.localFilters, [key]: value },
+  }));
+  getCharacters();
+}
+
+export async function getCharacters() {
+  const {
+    name,
+    urlPage,
+    apiFilters: { status, gender, species },
+  } = useCharacters.getState();
+
+  const urlPage1 = urlPage * 2 - 1;
+  const urlPage2 = urlPage * 2;
+
+  const querys = `${name ? `&name=${name}` : ""}${status === "All status" ? "" : `&status=${status}`}${gender === "All gender" ? "" : `&gender=${gender}`}${species === "All species" ? "" : `&species=${species}`}`;
+
+  const urls = [
+    `https://rickandmortyapi.com/api/character?page=${urlPage1}${querys}`,
+    `https://rickandmortyapi.com/api/character?page=${urlPage2}${querys}`,
+  ];
+
+  const responses = await Promise.all(urls.map((url) => fetch(url)));
+
+  if (responses.some((response) => !response.ok)) {
+    set({ characters: [] });
+    set({ info: null });
+    set({ totalPages: 0 });
+
+    return;
+  }
+
+  const data = await Promise.all(responses.map((res) => res.json()));
+  const allCharacters = data.flatMap((page) => (page.results ? page.results : []));
+  const allInfo = data[0].info;
+
+  useCharacters.setState({ characters: allCharacters });
+  useCharacters.setState({ info: allInfo });
+  useCharacters.setState({ totalPages: Math.ceil(allInfo?.count / 40) });
+}
+
+export function resetFilters() {
+  const { apiFilters, localFilters } = useCharacters.getState();
+
+  const isApiDefault =
+    apiFilters.status === defApiFilters.status &&
+    apiFilters.gender === defApiFilters.gender &&
+    apiFilters.species === defApiFilters.species;
+
+  const isLocalDefault = localFilters.sorting === defLocalFilters.sorting;
+
+  if (isApiDefault && isLocalDefault) return;
+
+  useCharacters.setState({ apiFilters: defApiFilters, localFilters: defLocalFilters });
+
+  getCharacters();
+}
--- src/store/favoritesCharacters.js
+++ src/store/favoritesCharacters.js
@@ -1,7 +1,63 @@
 import { create } from "zustand";
 
-export const useFavoriteCharacters = create((set) => ({
-  favoritesCharacters: [],
+export const statusOptions = ["All status", "Alive", "Dead", "unknown"];
+export const genderOptions = ["All gender", "Male", "Female", "Genderless", "unknown"];
+export const speciesOptions = [
+  "All species",
+  "Human",
+  "Alien",
+  "Humanoid",
+  "Poopybutthole",
+  "Mythological Creature",
+  "Animal",
+  "Robot",
+  "Cronenberg",
+  "Disease",
+  "unknown",
+];
+export const sortingOptions = ["Without sorting", "Name A-Z", "Name Z-A"];
 
-  setFavoritesCharacters: (favoritesCharacters) => set({ favoritesCharacters }),
+const defFavCharFilters = {
+  favCharStatus: "All status",
+  favCharGender: "All gender",
+  favCharSpecies: "All species",
+  favCharSorting: "Without sorting",
+};
+
+export const useFavoriteCharacters = create(() => ({
+  favoritesCharacters: [],
+  nameFavChar: "",
+  statusOptions,
+  genderOptions,
+  speciesOptions,
+  sortingOptions,
+  favCharFilters: defFavCharFilters,
 }));
+
+export function setNameFavChar(nameFavChar) {
+  useFavoriteCharacters.setState({ nameFavChar });
+}
+
+export function setFavoritesCharacters(favoritesCharacters) {
+  useFavoriteCharacters.setState({ favoritesCharacters });
+}
+
+export function setFavCharFilter(key, value) {
+  useFavoriteCharacters.setState((state) => ({
+    favCharFilters: { ...state.favCharFilters, [key]: value },
+  }));
+}
+
+export function resetFilters() {
+  const { favCharFilters } = useFavoriteCharacters.getState();
+
+  const isDefFilters =
+    favCharFilters.favCharStatus === defFavCharFilters.favCharStatus &&
+    favCharFilters.favCharGender === defFavCharFilters.favCharGender &&
+    favCharFilters.favCharSpecies === defFavCharFilters.favCharSpecies &&
+    favCharFilters.favCharSorting === defFavCharFilters.favCharSorting;
+
+  if (isDefFilters) return;
+
+  useFavoriteCharacters.setState({ favCharFilters: defFavCharFilters });
+}
