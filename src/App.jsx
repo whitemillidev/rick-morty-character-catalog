2a5a9abb73ee1016e83aa483dcf7ba7a563d0075
Ramil <Ramilmys@gmail.com>
2026-03-12 04:00:03 +0700

    Главный файл полностью дописан

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,17 @@ import InfoCharacters from "./components/InfoCharacters";
 import SearchCharacters from "./components/SearchCharacters";
 import SortingCharacters from "./components/SortingCharacters";
 import ActiveCard from "./components/ActiveCard";
-import { useCharacters } from "./store/characters";
 import Pagination from "./components/Pagination";
 import CharactersList from "./components/CharactersList";
 
 export default function RickMortyCharacterCatalog() {
-  const activeCard = useCharacters((state) => state.activeCard);
-
   return (
     <div className={styles[`app_character-catalog`]}>
       <header className={styles["app_header"]}>
         <h1 className={styles["app-title"]}>Rick & Morty — character catalog</h1>
       </header>
 
-      <main
-        className={activeCard === null ? styles["app_main-container-active-card-hidden"] : styles["app_main-container"]}
-      >
+      <main className={styles["app_main-container"]}>
         <InfoCharacters />
         <SearchCharacters />
         <SortingCharacters />
