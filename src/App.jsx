a185d9972f7e1a033de38ea7ccf6fd9fab852829
Ramil <Ramilmys@gmail.com>
2026-09-16 19:44:20 +0700

    Фикс для деплоя

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function RickMortyCharacterCatalog() {
       {/* <TestUseDocumentTitle /> */}
       {/* <TestUseToggle /> */}
       {/* <TestUseDebounce /> */}
-      <TestUseThrottle />
+      {/* <TestUseThrottle /> */}
     </div>
   );
 }
