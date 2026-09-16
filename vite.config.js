bfa2b47f0473110d8c1a895f7799898cd1adabc0
Ramil <Ramilmys@gmail.com>
2026-09-16 18:34:15 +0700

    Фикс названия пути

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: `/rick-morty-character-catalog/`,
+  base: "/Rick-Morty-character-catalog-/",
   plugins: [react()],
 });
